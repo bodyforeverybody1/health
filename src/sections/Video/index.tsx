7438217f22b6842dd4ed632e743e8e4bc2df6f49
health <bodyforeverybody1@gmail.com>
2024-10-29 16:34:47 +0400

    added autoplay for video

--- src/sections/Video/index.tsx
+++ src/sections/Video/index.tsx
@@ -1,14 +1,44 @@
 import { Container } from "../../styled.root.ts";
 import { Title } from "./styled.ts";
+import { useEffect, useRef } from "react";
 
 const Video: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            videoRef.current?.play();
+          } else {
+            videoRef.current?.pause();
+          }
+        });
+      },
+      {
+        threshold: 0.5, // Воспроизведение начнется, когда хотя бы 50% видео будет в зоне видимости
+      },
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, []);
+
   return (
     <section id="video">
       <Container>
         <div>
           <Title>проморолик</Title>
           <div>
-            <video width="100%" height="100%" controls>
+            <video ref={videoRef} width="100%" height="100%" controls>
               <source src="/video/video.mp4" type="video/mp4" />
               Your browser does not support the video tag.
             </video>
